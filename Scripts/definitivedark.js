b5c24c52783f43e886604dc0289b925ea61269d0
unfotografomascuu <unfotografomascuu@gmail.com>
2021-12-07 00:46:55 -0700

    Optimizaciones y OG

--- Scripts/definitivedark.js
+++ Scripts/definitivedark.js
@@ -6,36 +6,28 @@ function DarkMode() {
     if (amode.checked == true) {
         document.body.classList.remove("theme-light", "theme-dark");
         document.body.classList.add(`theme-auto`);
-        console.log("Modo Automatico.");
     }
 
     if (lmode.checked == true) {
         document.body.classList.remove("theme-auto", "theme-dark");
         document.body.classList.add(`theme-light`);
-        console.log("Modo Claro.");
-    }
+        }
 
     if (dmode.checked == true) {
         document.body.classList.remove("theme-light", "theme-auto");
         document.body.classList.add(`theme-dark`);
-        console.log("Modo Oscuro.");
     }
 
-    document.addEventListener("DOMContentLoaded", () => {
-        document.querySelector("#theme").addEventListener("change", function () {
-            applyTheme(this.value);
-        });
-    });
 
     document.addEventListener("DOMContentLoaded", () => {
         const savedTheme = localStorage.getItem("theme") || "auto";
-
+    
         applyTheme(savedTheme);
-
+    
         for (const optionElement of document.querySelectorAll("#theme option")) {
             optionElement.selected = savedTheme === optionElement.value;
         }
-
+    
         document.querySelector("#theme").addEventListener("change", function () {
             localStorage.setItem("theme", this.value);
             applyTheme(this.value);
